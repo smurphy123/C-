83ff6fd2811f78e5b099ae63d936d049195b2e04
smurphy123 <smurphysmail@gmail.com>
2021-04-25 16:06:42 -0400

    Repo Update

---
+++ TournamentTracker/TornamentTracker/TrackerUI/TournamentDashboardForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TrackerUI
+{
+    public partial class TournamentDashboardForm : Form
+    {
+        public TournamentDashboardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void headerLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
