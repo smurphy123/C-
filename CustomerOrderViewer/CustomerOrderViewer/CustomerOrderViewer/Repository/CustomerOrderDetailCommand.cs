bb553f0838c17feea922c5697116a1d094c1917b
smurphy123 <smurphysmail@gmail.com>
2021-04-26 20:46:20 -0400

    Repo Update

--- CustomerOrderViewer/CustomerOrderViewer/CustomerOrderViewer/Repository/CustomerOrderDetailCommand.cs
+++ CustomerOrderViewer/CustomerOrderViewer/CustomerOrderViewer/Repository/CustomerOrderDetailCommand.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using CustomerOrderViewer.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace CustomerOrderViewer.Repository
 {
     class CustomerOrderDetailCommand
     {
+        private string _connectionString;
+
+        public CustomerOrderDetailCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public IList<CustomerOrderDetailModel> GetLsit()
+        {
+            List<CustomerOrderDetailModel> customerOrderDetailModels = new List<CustomerOrderDetailModel>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+
+            }
+
+            return customerOrderDetailModels;
+        }
     }
 }
