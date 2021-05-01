735687d423869e43b5bfd3ff1435b09beb35990a
smurphy123 <smurphysmail@gmail.com>
2021-05-01 11:24:06 -0400

    Repo Update

---
+++ SimpleCalculator/SimpleCalculator/SimpleCalculator/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    public class Person
+    {
+        private int _age = 25;
+        public int getAge()
+        {
+            return _age;
+        }
+
+        public void setAge(int newAge)
+        {
+            _age = newAge;
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person();
+        }
+    }
+}
