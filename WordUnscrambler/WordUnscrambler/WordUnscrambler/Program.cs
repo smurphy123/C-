e0b9c5e63324bd651ad3fc9893d8a443ac9054d3
smurphy123 <smurphysmail@gmail.com>
2021-05-03 18:19:18 -0400

    Repo Update

---
+++ WordUnscrambler/WordUnscrambler/WordUnscrambler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myFirstList = new List<int>();
+
+            Console.WriteLine(myFirstList.Count());
+
+            myFirstList.Add(10);
+            myFirstList.Add(5);
+
+            Console.WriteLine(myFirstList.Count());
+        }
+    }
+}
