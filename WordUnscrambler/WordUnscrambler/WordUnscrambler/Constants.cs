07ca5e71459f862e1773758d3ece60020c32274d
smurphy123 <smurphysmail@gmail.com>
2021-05-08 21:46:24 -0400

    Repo Update

---
+++ WordUnscrambler/WordUnscrambler/WordUnscrambler/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordUnscrambler
+{
+    class Constants
+    {
+        public const string OptionsOnHowToEnterScrambledWords = "Entered scrambled word(s) manually or as a file: F - file/M - manual";
+        public const string OptionsOnContinuingTheProgram = "Would you like to continue? Y/N";
+        
+        public const string EnterScrambledWordsViaFile = "Enter full path including the file name: ";
+        public const string EnterScrambledWordsManually = "Enter word(s) manually (separated by commas): ";
+        public const string EnterScrambledWordsOptionnotRecognized = "The option you entered was not recognized.";
+        
+        public const string ErrorScrambledWordsCannotBeLoaded = "Scrambled words cannot be loaded because there was an error: ";
+        public const string ErrorProgramWillBeTerminated = "The program will be terminated: ";
+
+        public const string MatchFound = "MATCH FOUND FOR {0}: {1}";
+        public const string MatchNotFound = "NO MATCHES HAVE BEEN FOUND";
+
+        public const string Yes = "Y";
+        public const string NO = "N";
+    }
+}
